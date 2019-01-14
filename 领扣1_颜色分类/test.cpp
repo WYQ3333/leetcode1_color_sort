8a508071a6ea4d1464da76e56022a0c6b3c7f157
浅忆梦微凉 <3538922590@qq.com>
2019-01-14 17:10:20 +0800

    第二种方法

--- 领扣1_颜色分类/test.cpp
+++ 领扣1_颜色分类/test.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 
-class Solution {
+class Solution1 {
 public:
 	void swap(int& p, int& q){
 		int cur = p;
@@ -35,11 +35,44 @@ public:
 	
 };
 
+class Solution2 {
+public:
+	void sortColors(vector<int>& nums) {
+		//Ë«Ö¸Õë
+		int low = 0, high = nums.size() - 1;
+		int i = 0;
+		while (i <= high){
+			if (nums[i] == 0){
+				int tmp = nums[i];
+				nums[i] = nums[low];
+				nums[low] = tmp;
+				++low; ++i;
+			}
+			else if (nums[i] == 1){
+				++i;
+			}
+			else if (i <= high && nums[i] == 2){
+				int tmp = nums[i];
+				nums[i] = nums[high];
+				nums[high] = tmp;
+				--high;
+			}
+		}
+	}
+};
+
 int main(){
 	vector<int> array = { 0, 1, 2, 2, 1, 1, 1, 0, 0, 2 };
-	Solution s;
-	s.sortColors(array);
+	Solution1 s1;
+	s1.sortColors(array);
 
+	Solution1 s2;
+	s2.sortColors(array);
+	int i = 0;
+	for (i = 0; i < array.size(); ++i){
+		cout << array[i] << " ";
+	}
+	cout << endl;
 	system("pause");
 	return 0;
 }
